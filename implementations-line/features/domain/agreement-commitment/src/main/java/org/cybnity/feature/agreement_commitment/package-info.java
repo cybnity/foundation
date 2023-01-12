25099184d56919ee266f5bbdbcccb28f7871ec4f
Olivier Lemée <olivier@agnet.fr>
2023-01-12 15:54:24 +0100

    structure of feature module

---
+++ implementations-line/features/domain/agreement-commitment/src/main/java/org/cybnity/feature/agreement_commitment/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * Security feature module regarding a specific domain.
+ * <p>
+ * Specialized domain implementation module of CYBNITY feature packaged and deployable as micro-service (Real-Time Computation Unit).
+ * </p>
+ *
+ * @since 0.1.0
+ * @author o.lemee
+ * @version 0.1.0
+ */
+package org.cybnity.feature.agreement_commitment;
