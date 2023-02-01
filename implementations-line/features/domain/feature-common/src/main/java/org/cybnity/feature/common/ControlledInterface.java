35e83bf9c4dcbacad9d36dc1435f24d7aa4cf7bd
Olivier Lemée <olivier@agnet.fr>
2023-02-01 16:27:59 +0100

    add basic generic and common classes reusable by any features implementation

---
+++ implementations-line/features/domain/feature-common/src/main/java/org/cybnity/feature/common/ControlledInterface.java
@@ -0,0 +1,13 @@
+package org.cybnity.feature.common;
+
+/**
+ * Capability allowed to make interaction with one or several resource types
+ * (e.g via a remote call on a protocol, via specific data structure exchange).
+ * Can be considered as a mean (e.g Application Public Interface (API)) to apply
+ * changes on a system, an application, an information or any type of resource
+ * authorized and controlled at the runtime (e.g via access control, permissions
+ * check...).
+ */
+public interface ControlledInterface {
+
+}
