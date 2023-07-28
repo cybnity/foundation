d161b333bb167b061bfe2f4fa0110966c67feb51
Olivier Lemée <olivier@agnet.fr>
2023-07-28 14:51:02 +0200

    helm DIS project (charts) migrate to CYBNITY helm charts repository

--- implementations-line/systems/modules/local-env/cluster/cluster-node-labels-add.sh
+++ implementations-line/systems/modules/local-env/cluster/cluster-node-labels-add.sh
@@ -3,9 +3,9 @@
 # BE CAREFULL, MINIKUBE LOST LABELS ON NODES WHEN RESTARTED
 echo "Add labels to the cluster nodes"
 
-kubectl label nodes local-env4 layer=user-interfaces-area
-kubectl label nodes local-env4-m02 layer=domains-io-area
-kubectl label nodes local-env4-m03 layer=domains-area
-kubectl label nodes local-env4-m04 layer=infrastructure-services-area
+kubectl label nodes local-env4 cybnity.io/user-interfaces-area=true
+kubectl label nodes local-env4-m02 cybnity.io/domains-io-area=true
+kubectl label nodes local-env4-m03 cybnity.io/domains-area=true
+kubectl label nodes local-env4-m04 cybnity.io/infrastructure-services-area=true
 
 kubectl get nodes --show-labels
