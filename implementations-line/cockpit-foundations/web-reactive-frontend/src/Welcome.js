435efff6e40cb68c6df06bbb7a27468cea84840e
Olivier Lemée <olivier@agnet.fr>
2023-08-24 16:45:15 +0200

    styling and bootstrap configuration

--- implementations-line/cockpit-foundations/web-reactive-frontend/src/Welcome.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const Welcome = () => (
-      <div className="Welcome">
-        <p>This is your public-facing component.</p>
-      </div>
-);
-export default Welcome;
