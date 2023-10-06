76b20ffbac3d3e7fefba6a16b131524d26a31a00
Olivier Lemée <olivier@agnet.fr>
2023-10-06 16:47:17 +0200

    logo button customization

--- implementations-line/cockpit-foundations/web-reactive-frontend/src/features/cockpits/NavBarBrandIcon.js
+++ implementations-line/cockpit-foundations/web-reactive-frontend/src/features/cockpits/NavBarBrandIcon.js
@@ -2,6 +2,7 @@ import React from "react";
 import logo from "../../media/cybnity-gorilla-light.svg";
 import {Col, Container, Image, Row} from "react-bootstrap";
 import {HiMenu} from "react-icons/hi";
+import {Button} from "react-bootstrap/";
 
 /**
  * Icon of cockpit screens manager.
@@ -14,15 +15,18 @@ export default function NavBarBrandIcon({logoOnClickHandler, menuOnClickHandler}
         <Container className="cockpit-tab-container" fluid>
             <Row className="justify-content-xs-center" xs={2} md={2} lg={2}>
                 <Col>
-                    <Image className="tab-bar-logo"
-                           alt="CYBNITY logo"
-                           src={logo} onClick={logoOnClickHandler}
-                    />
+                    <Button className="tab-bar-logo-button">
+                        <Image className="tab-bar-logo"
+                               alt="CYBNITY logo"
+                               src={logo} onClick={logoOnClickHandler}
+                        /></Button>
                 </Col>
                 <Col xs="auto">
-                    <HiMenu className="tab-bar-icon" onClick={menuOnClickHandler}/></Col>
+                    <HiMenu className="tab-bar-icon" onClick={menuOnClickHandler}/>
+                </Col>
             </Row>
         </Container>
-    );
+    )
+        ;
 
 };
