c8e236d00e6e68b47f45f2502333969d891b478c
Olivier Lemée <olivier@agnet.fr>
2023-12-20 08:42:46 +0100

    Add conformity violation case about unsupported event type

--- implementations-line/framework/domain/src/main/java/org/cybnity/framework/domain/ConformityViolation.java
+++ implementations-line/framework/domain/src/main/java/org/cybnity/framework/domain/ConformityViolation.java
@@ -12,6 +12,11 @@ public enum ConformityViolation {
     UNIDENTIFIED_EVENT_TYPE,
 
     /**
+     * Type of fact event is not supported (e.g regarding any applicative or collaboration rule).
+     */
+    UNSUPPORTED_EVENT_TYPE,
+
+    /**
      * A message structure is not supported (e.g does not satisfy the JSON/POJO mapping requirements).
      */
     UNSUPPORTED_MESSAGE_STRUCTURE,
@@ -19,7 +24,7 @@ public enum ConformityViolation {
     /**
      * None capability is identified as able to perform an event type processing.
      */
-    UNPROCESSABLE_EVENT_TYPE
+    UNPROCESSABLE_EVENT_TYPE,
 
     ;
 }
