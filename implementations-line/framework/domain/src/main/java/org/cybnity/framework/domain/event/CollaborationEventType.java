c392e847e72b3a88e81c06e1bf3bb8540b8cca96
Olivier Lemée <olivier@agnet.fr>
2024-01-16 08:02:00 +0100

    add event support about dynamic renewal of processing units presence announces

--- implementations-line/framework/domain/src/main/java/org/cybnity/framework/domain/event/CollaborationEventType.java
+++ implementations-line/framework/domain/src/main/java/org/cybnity/framework/domain/event/CollaborationEventType.java
@@ -11,6 +11,12 @@ public enum CollaborationEventType implements IEventType {
     PROCESSING_UNIT_PRESENCE_ANNOUNCED,
 
     /**
+     * Event about a request to processing unit for send presence announce.
+     * This type of event is usable by any domain gateway which need to be notified about current active processing units (e.g features).
+     */
+    PROCESSING_UNIT_PRESENCE_ANNOUNCE_REQUESTED,
+
+    /**
      * Event about a registered routing paths (e.g path to PU entrypoint channel) to a processing unit (e.g considered as eligible to delegation of command events treatment).
      */
     PROCESSING_UNIT_ROUTING_PATHS_REGISTERED;
