2cef723315153dc07ac0d58d808004be12cb1ed9
Olivier Lemée <olivier@agnet.fr>
2024-01-31 11:41:39 +0100

    refactoring

--- implementations-line/infrastructures/integration/system/users-interactions-broker/uib-adapters/uis-adapter-api/src/main/java/org/cybnity/infrastructure/technical/message_bus/adapter/api/event/ProcessingUnitPresenceAnnouncedEventFactory.java
+++ implementations-line/infrastructures/integration/system/users-interactions-broker/uib-adapters/uis-adapter-api/src/main/java/org/cybnity/infrastructure/technical/message_bus/adapter/api/event/ProcessingUnitPresenceAnnouncedEventFactory.java
@@ -11,10 +11,12 @@ import org.cybnity.framework.domain.event.ProcessingUnitPresenceAnnounced;
 import org.cybnity.framework.domain.model.DomainEntity;
 import org.cybnity.framework.immutable.BaseConstants;
 import org.cybnity.framework.immutable.EntityReference;
+import org.cybnity.framework.immutable.Identifier;
+import org.cybnity.framework.immutable.ImmutabilityException;
 import org.cybnity.infrastructure.technical.message_bus.adapter.api.ICapabilityChannel;
 
+import java.util.LinkedHashSet;
 import java.util.Map;
-import java.util.UUID;
 
 /**
  * Factory of processing unit operational status change announcing event.
@@ -24,29 +26,37 @@ public class ProcessingUnitPresenceAnnouncedEventFactory {
     /**
      * Prepare a domain event relative to a processing unit presence status that was changed.
      *
-     * @param supportedEventTypesToRoutingPath Mandatory set of routing map regarding supported event types by PU, and entrypoint channel paths.
+     * @param supportedEventTypesToRoutingPath Optional set of routing map regarding supported event types by the announced PU, and including entrypoint channel paths.
      * @param puServiceName                    Optional logical name of the processing unit that is able to treat the announced event types.
      * @param currentStatus                    Mandatory current status of presence which is defined by the built announce.
      * @return A prepared instance of change event.
      * @throws IllegalArgumentException When mandatory parameter is missing.
+     * @throws ImmutabilityException When impossible read of priorEventRef identity.
      */
-    public ProcessingUnitPresenceAnnounced create(Map<IEventType, ICapabilityChannel> supportedEventTypesToRoutingPath, String puServiceName, EntityReference priorEventRef, IPresenceObservability.PresenceState currentStatus) throws IllegalArgumentException {
-        if (supportedEventTypesToRoutingPath == null || supportedEventTypesToRoutingPath.isEmpty())
-            throw new IllegalArgumentException("supportedEventTypesToRoutingPath parameter is required and shall not be empty!");
-        if (currentStatus == null) throw new IllegalArgumentException("currentStatus parameter is required!");
+    public ProcessingUnitPresenceAnnounced create(Map<IEventType, ICapabilityChannel> supportedEventTypesToRoutingPath, String puServiceName, EntityReference priorEventRef, IPresenceObservability.PresenceState currentStatus) throws IllegalArgumentException, ImmutabilityException {
+        if (currentStatus == null) throw new IllegalArgumentException("Current presence status parameter is required!");
 
-        // Create new event identity
-        String uidValue = UUID.randomUUID().toString();
-        DomainEntity identifiedBy = new DomainEntity(new IdentifierStringBased(BaseConstants.IDENTIFIER_ID.name(), uidValue));
+        // Create new event identity based on current time, and random technical value
+        String uidValue = CorrelationIdFactory.generate(null);
+        LinkedHashSet<Identifier> identificationValues = new LinkedHashSet<>();
+        identificationValues.add(new IdentifierStringBased(BaseConstants.IDENTIFIER_ID.name(), uidValue)); // Basic technical id of event
+        if (priorEventRef!=null) {
+            // Add complementary identifier of prior event
+            identificationValues.add(priorEventRef.getEntity().identified());
+        }
+        // Prepare announce event final identity
+        DomainEntity identifiedBy = new DomainEntity(identificationValues);
 
         // Prepare announcing event
         ProcessingUnitPresenceAnnounced announced = new ProcessingUnitPresenceAnnounced(identifiedBy, CollaborationEventType.PROCESSING_UNIT_PRESENCE_ANNOUNCED);
 
-        // Create a description of each PU supported event type and routing path
-        for (Map.Entry<IEventType, ICapabilityChannel> route : supportedEventTypesToRoutingPath.entrySet()) {
-            if (route.getKey() != null && route.getValue() != null) {
-                // Add definition of routing map supporting a type of event which can be treated by the PU
-                announced.appendEventRoutingPath(new Attribute(route.getKey().name(), route.getValue().shortName()));
+        if (supportedEventTypesToRoutingPath != null) {
+            // Create a description of each PU supported event type and routing path
+            for (Map.Entry<IEventType, ICapabilityChannel> route : supportedEventTypesToRoutingPath.entrySet()) {
+                if (route.getKey() != null && route.getValue() != null) {
+                    // Add definition of routing map supporting a type of event which can be treated by the PU
+                    announced.appendEventRoutingPath(new Attribute(route.getKey().name(), route.getValue().shortName()));
+                }
             }
         }
 
