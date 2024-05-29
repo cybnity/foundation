a81a8ffb71db61ce0f68b73348a4152f2a36429b
Olivier Lemée <olivier@agnet.fr>
2024-05-29 19:51:24 +0200

    remove local-dev unusable sh script

--- implementations-line/systems/modules/local-env/services/knowledge-repository-server/start-janusgraph-server.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-# Installation of latest release of JanusGraph services with Cassandra storage backend
-helm install knr-system -f values-janusgraph.yaml oci://registry-1.docker.io/bitnamicharts/janusgraph
