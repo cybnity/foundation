c623a1c0d3d332c5f7ea8fbb739832e236e8c089
Olivier Lemée <olivier@agnet.fr>
2024-08-19 12:55:41 +0200

    Introduce standardized attribute names relative to data view events (e.g reused when read-model elements have been updated)

--- implementations-line/infrastructures/registry/system/knowledge-repository-server/knr-adapters/knr-adapter-api/src/main/java/org/cybnity/infrastructure/technical/registry/adapter/api/event/DataViewAttributeName.java
+++ implementations-line/infrastructures/registry/system/knowledge-repository-server/knr-adapters/knr-adapter-api/src/main/java/org/cybnity/infrastructure/technical/registry/adapter/api/event/DataViewAttributeName.java
@@ -6,5 +6,25 @@ package org.cybnity.infrastructure.technical.registry.adapter.api.event;
  */
 public enum DataViewAttributeName {
 
-    ;
+    /**
+     * Unique technical identifier of a read-model data view.
+     * For example, can be valued by a Vertex id.
+     */
+    DATAVIEW_UID,
+
+    /**
+     * Label defining a node type relative to a data view (e.g equals to a Vertex nature).
+     * It's define a type of node based on an unique label supported into a perimeter of nodes (e.g domain based).
+     */
+    DATAVIEW_NODE_LABEL,
+
+    /**
+     * Unique identifier of a domain abject (e.g any aggregate identifier).
+     */
+    DOMAIN_OBJECT_UID,
+
+    /**
+     * A specific property defining a unique logical name assigned to a node instance.
+     */
+    DATAVIEW_NODE_NAME
 }
