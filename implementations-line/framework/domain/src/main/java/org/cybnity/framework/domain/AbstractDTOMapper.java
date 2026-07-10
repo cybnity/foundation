d5b05a3e4f2966df425747bd9bdc09017f896cd9
Olivier Lemée <olivier@agnet.fr>
2026-07-10 09:32:01 +0200

    Update AbstractDTOMapper.java

--- implementations-line/framework/domain/src/main/java/org/cybnity/framework/domain/AbstractDTOMapper.java
+++ implementations-line/framework/domain/src/main/java/org/cybnity/framework/domain/AbstractDTOMapper.java
@@ -1,7 +1,7 @@
 package org.cybnity.framework.domain;
 
 /**
- * Implementation class type allowing instantiation of DataTransferObject (e.g degraded data view) from other type of object (e.g domain aggregate object).
+ * Implementation class type allowing instantiation of DataTransferObject (e.g; degraded data view) from other type of object (e.g; domain aggregate object).
  * This type of mapper shall be customized by subclass according to the type of DTO expected as output of structure conversion.
  */
 public abstract class AbstractDTOMapper<T extends DataTransferObject> implements IDataTransferObjectMapping<T> {
